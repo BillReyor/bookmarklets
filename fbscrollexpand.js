6ff2b53477ba5325eedeca9fff3eaebd074c3339
Bill Reyor <87031733+BillReyor@users.noreply.github.com>
2023-04-16 10:39:29 -0400

    Function updates
    Removing the counter, adding pause, resume, and stop functionality using the 'p', 'r', and 's' keys, and attempting to prevent the Facebook page from automatically reloading.

--- fbscrollexpand.js
+++ fbscrollexpand.js
@@ -1,9 +1,15 @@
 javascript:(function() {
-  var counter = 0;
+  var isPaused = false;
+  var shouldStop = false;
+
+  function preventAutoReload() {
+    window.onbeforeunload = function() {
+      return "Are you sure you want to leave?";
+    };
+  }
 
   function expandCommentsAndReplies() {
     var clickables = Array.from(document.querySelectorAll('[aria-label="View more comments"], [aria-label="View more replies"], [aria-label="See hidden replies"]'));
-
     for (var i = 0; i < clickables.length; i++) {
       clickables[i].click();
     }
@@ -14,13 +20,26 @@ javascript:(function() {
   }
 
   function autoExpandAndScroll() {
-    if (counter < 5) {
-      expandCommentsAndReplies();
-      scrollToBottom();
-      counter++;
+    if (!shouldStop) {
+      if (!isPaused) {
+        expandCommentsAndReplies();
+        scrollToBottom();
+      }
       setTimeout(autoExpandAndScroll, 1000);
     }
   }
 
+  function handleKeyPress(e) {
+    if (e.key === 'p') {
+      isPaused = !isPaused;
+    } else if (e.key === 'r') {
+      isPaused = false;
+    } else if (e.key === 's') {
+      shouldStop = true;
+    }
+  }
+
+  window.addEventListener('keypress', handleKeyPress);
+  preventAutoReload();
   autoExpandAndScroll();
 })();
